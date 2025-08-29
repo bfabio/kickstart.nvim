1021c79248387cb0a34cfb489d4959f0ab9c117b
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2025-08-29 14:52:31 +0300

    fix: adjust after 'mini.nvim' transfer to 'nvim-mini' org

--- init.lua
+++ init.lua
@@ -997,7 +997,7 @@ require('lazy').setup({
   },
 
   { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     config = function()
       -- Better Around/Inside textobjects
       --
@@ -1028,7 +1028,7 @@ require('lazy').setup({
       statusline.section_location = function() return '%2l:%-2v' end
 
       -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
+      --  Check out: https://github.com/nvim-mini/mini.nvim
     end,
   },
   { -- Highlight, edit, and navigate code
